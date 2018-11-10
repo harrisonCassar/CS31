e50ea80c5f4772b667eb93f8742e5b12650c1dcf
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2018-11-10 13:41:02 -0800

    Begin program; no-content skeleton program

---
+++ P5/rate.cpp
@@ -0,0 +1,47 @@
+/****************************************************************************
+	Harrison Cassar
+	ID: 505114980
+	Discussion 1F
+	TA: Tao Zhou
+
+	Project 5 for CS31 with Professor Smallberg
+
+	Last Edit: 11/11/2018
+
+	Purpose: Code serves to properly format word patterns and identifies the
+	rating that a specific document matches with some given set of patterns.
+
+****************************************************************************/
+
+//define statement included to remove certain flags with VS
+#define _CRT_SECURE_NO_WARNINGS
+
+//include of libraries & namespaces
+#include <iostream>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+using namespace std;
+
+//declare global constants
+const int MAX_WORD_LENGTH = ;
+
+//required function prototypes
+int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1], int separation[], int nPatterns);
+int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1], const char word2[][MAX_WORD_LENGTH+1], const int separation[], int nPatterns);
+
+//testing processes of coded functions
+int main()
+{
+	return 0;
+}
+
+int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1], int separation[], int nPatterns)
+{
+	
+}
+
+int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1], const char word2[][MAX_WORD_LENGTH+1], const int separation[], int nPatterns)
+{
+	
+}
